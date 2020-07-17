724d9e57b8bfe40049452a9173e189b961723e8c
Jed <jed324@gmail.com>
2020-07-17 00:06:51 -0700

    hbridge test

---
+++ ENPH253/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#define MOTOR_A PA_2 
+#define MOTOR_B PA_3
+#define RESO RESOLUTION_10B_COMPARE_FORMAT
+#define PWMFREQ 1000
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(MOTOR_A, OUTPUT);
+  pinMode(MOTOR_B, OUTPUT);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  pwm_start(MOTOR_A, PWMFREQ, 512, RESO);
+  pwm_start(MOTOR_B, PWMFREQ, 0, RESO);
+
+  delay(1000);
+
+  pwm_start(MOTOR_A, PWMFREQ, 0, RESO);
+  pwm_start(MOTOR_B, PWMFREQ, 0, RESO);
+
+  delay(1000);
+
+  pwm_start(MOTOR_A, PWMFREQ, 0, RESO);
+  pwm_start(MOTOR_B, PWMFREQ, 512, RESO);
+  
+  delay(1000);
+}
