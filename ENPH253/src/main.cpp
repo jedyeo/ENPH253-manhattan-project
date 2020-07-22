e65abd1393773ec585a864732b72482ec7658e08
Jed <jed324@gmail.com>
2020-07-22 02:30:14 -0700

    sonar header and refactor pin defines

--- ENPH253/src/main.cpp
+++ ENPH253/src/main.cpp
@@ -1,5 +1,9 @@
+#pragma once
+
 #include <Arduino.h>
 #include <Servo.h>
+#include "pin_define.h"
+#include "sonar.h"
 
 // Pin Definitions
 // y A9 g A10
@@ -7,7 +11,6 @@
 #define MOTOR_R2 PB_9 // rev 
 #define MOTOR_L1 PB_6 // forward
 #define MOTOR_L2 PB_7 // rev
-#define RAMP_SERVO PA2 
 
 #define RESO RESOLUTION_10B_COMPARE_FORMAT
 #define PWMFREQ 1000
@@ -15,98 +18,102 @@
 Servo rampServo;
 
 void stopMotor() {
-  pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
-  pwm_start(MOTOR_R2, PWMFREQ, 0, RESO);
-  pwm_start(MOTOR_L1, PWMFREQ, 0, RESO);
-  pwm_start(MOTOR_L2, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_R2, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_L1, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_L2, PWMFREQ, 0, RESO);
 }
 
 void moveForward(int time) {
-  pwm_start(MOTOR_R1, PWMFREQ, 1023, RESO); // go forward
-  pwm_start(MOTOR_R2, PWMFREQ, 0, RESO); // do not reverse
-  pwm_start(MOTOR_L1, PWMFREQ, 1023, RESO); // go forward
-  pwm_start(MOTOR_L2, PWMFREQ, 0, RESO); // do not reverse
-  delay(time);
-  stopMotor();
+    pwm_start(MOTOR_R1, PWMFREQ, 1023, RESO); // go forward
+    pwm_start(MOTOR_R2, PWMFREQ, 0, RESO); // do not reverse
+    pwm_start(MOTOR_L1, PWMFREQ, 1023, RESO); // go forward
+    pwm_start(MOTOR_L2, PWMFREQ, 0, RESO); // do not reverse
+    delay(time);
+    stopMotor();
 }
 
 void moveBackwards(int time) {
-  pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
-  pwm_start(MOTOR_R2, PWMFREQ, 1023, RESO); 
-  pwm_start(MOTOR_L1, PWMFREQ, 0, RESO); 
-  pwm_start(MOTOR_L2, PWMFREQ, 1023, RESO); 
-  delay(time);
-  stopMotor();
+    pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_R2, PWMFREQ, 1023, RESO); 
+    pwm_start(MOTOR_L1, PWMFREQ, 0, RESO); 
+    pwm_start(MOTOR_L2, PWMFREQ, 1023, RESO); 
+    delay(time);
+    stopMotor();
 }
 
 void turnRight(int time) {
-  pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
-  pwm_start(MOTOR_R2, PWMFREQ, 1023, RESO); 
-  pwm_start(MOTOR_L1, PWMFREQ, 1023, RESO); 
-  pwm_start(MOTOR_L2, PWMFREQ, 0, RESO); 
-  delay(time);
-  stopMotor();
+    pwm_start(MOTOR_R1, PWMFREQ, 0, RESO);
+    pwm_start(MOTOR_R2, PWMFREQ, 1023, RESO); 
+    pwm_start(MOTOR_L1, PWMFREQ, 1023, RESO); 
+    pwm_start(MOTOR_L2, PWMFREQ, 0, RESO); 
+    delay(time);
+    stopMotor();
 }
 
 void turnLeft(int time) {
-  pwm_start(MOTOR_R1, PWMFREQ, 1023, RESO);
-  pwm_start(MOTOR_R2, PWMFREQ, 0, RESO); 
-  pwm_start(MOTOR_L1, PWMFREQ, 0, RESO); 
-  pwm_start(MOTOR_L2, PWMFREQ, 1023, RESO); 
-  delay(time);
-  stopMotor();
+    pwm_start(MOTOR_R1, PWMFREQ, 1023, RESO);
+    pwm_start(MOTOR_R2, PWMFREQ, 0, RESO); 
+    pwm_start(MOTOR_L1, PWMFREQ, 0, RESO); 
+    pwm_start(MOTOR_L2, PWMFREQ, 1023, RESO); 
+    delay(time);
+    stopMotor();
 }
 
 void depositCan() {
-  rampServo.write(60);
+    rampServo.write(60);
 }
 
 void retractRamp() {
-  rampServo.write(180);
+    rampServo.write(180);
 }
 
 void simpleTest() {
-  moveForward(750);
-  depositCan();
+    moveForward(750);
+    depositCan();
 
-  delay(1000);
-  retractRamp();
-  delay(10000);
+    delay(1000);
+    retractRamp();
+    delay(10000);
 }
 
 void dance() {
-  moveForward(500);
-  delay(500);
-  moveBackwards(500);
-  delay(500);
-  turnRight(500);
-  delay(500);
-  turnLeft(500);
-  delay(500);
+    moveForward(500);
+    delay(500);
+    moveBackwards(500);
+    delay(500);
+    turnRight(500);
+    delay(500);
+    turnLeft(500);
+    delay(500);
 }
 
 void setup() {
-  // put your setup code here, to run once:
+    // single motor pin modes
+    pinMode(MOTOR_R1, OUTPUT);
+    pinMode(MOTOR_R2, OUTPUT);
+    pinMode(MOTOR_L1, OUTPUT);
+    pinMode(MOTOR_L2, OUTPUT);
 
-  // single motor pin modes
-  pinMode(MOTOR_R1, OUTPUT);
-  pinMode(MOTOR_R2, OUTPUT);
-  pinMode(MOTOR_L1, OUTPUT);
-  pinMode(MOTOR_L2, OUTPUT);
+    // start at zero duty cycle
+    stopMotor();
 
-  // start at zero duty cycle
-  stopMotor();
+    // servo pin mode
+    rampServo.attach(RAMP_SERVO);
+    retractRamp();
 
-  // servo pin mode
-  rampServo.attach(RAMP_SERVO);
-  retractRamp();
+    // builtin LED for debug
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  // wait 5s to gather your thoughts and pray to andre
-  delay(5000);
+    // wait 5s to gather your thoughts and pray to andre
+    delay(5000);
 }
 
 void loop() {
-  //simpleTest();
-
-  
+    //simpleTest();
+    //dance();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
